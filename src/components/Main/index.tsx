c0e81300dc50c46c4b5fb905df66cebf8e92ba6c
Jon Martins <Jonathan.Martins@ctw.bmwgroup.com>
2022-10-14 14:45:44 +0100

    feature(map): adds map

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,5 +1,6 @@
 import { Card, Grid, Text } from "@nextui-org/react";
 import React, { FC, useContext } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { AppContext, useAppContext } from "../../context/App";
 
 export const Main: FC = (props) => {
@@ -8,7 +9,23 @@ export const Main: FC = (props) => {
   return (
     <div className="App-main">
       <Grid.Container gap={2} justify="center">
-        {results.map((result) => (
+        <MapContainer
+          center={[51.505, -0.09]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+
+        {/* {results.map((result) => (
           <Grid xs={4} key={result.id}>
             <Card css={{ h: "$24", $$cardColor: "$color$primary" }}>
               <Card.Body>
@@ -18,7 +35,7 @@ export const Main: FC = (props) => {
               </Card.Body>
             </Card>
           </Grid>
-        ))}
+        ))} */}
       </Grid.Container>
     </div>
   );
